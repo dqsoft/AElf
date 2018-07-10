372203e4927ae908f999423a435e71cb83ca38b1
samfrb <samos2x@outlook.com>
2018-07-10 18:21:25 +0800

    Wired up communication tester.

--- AElf.Network.Sim/Program.cs
+++ AElf.Network.Sim/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
 using System.Xml.Xsl;
 using AElf.Network.V2.DHT.Distance;
 
@@ -13,6 +16,20 @@ namespace AElf.Network.Sim
             Console.WriteLine("Example address #1 = " + addr1);
             Console.WriteLine("Example address #2 = " + addr2);
             Console.WriteLine("XOR distance = " + Distance.Calculate(addr1, addr2));
+
+            // Start comTester on a new thread
+            CommunicationTester comTester = new CommunicationTester();
+            Task.Run(() => comTester.Start());
+
+            Console.ReadKey();
+        }
+    }
+
+    public class CommunicationTester
+    {
+        public async void Start()
+        {
+            
         }
     }
 }
