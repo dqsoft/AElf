1b131d9ea1e35924fd212bc4ad939a9e57727292
samfrb <samos2x@outlook.com>
2018-07-11 18:12:58 +0800

    Added some testing code.

--- AElf.Network.Sim/Program.cs
+++ AElf.Network.Sim/Program.cs
@@ -1,8 +1,14 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Http.Headers;
+using System.Net.Sockets;
 using System.Runtime.InteropServices;
 using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 using System.Xml.Xsl;
+using AElf.Common.ByteArrayHelpers;
+using AElf.Network.V2.Connection;
 using AElf.Network.V2.DHT.Distance;
 
 namespace AElf.Network.Sim
@@ -19,7 +25,7 @@ namespace AElf.Network.Sim
 
             // Start comTester on a new thread
             CommunicationTester comTester = new CommunicationTester();
-            Task.Run(() => comTester.Start());
+            Task.Run(() => comTester.Start(args[0] == "1"));
 
             Console.ReadKey();
         }
@@ -27,9 +33,81 @@ namespace AElf.Network.Sim
 
     public class CommunicationTester
     {
-        public async void Start()
+        public async void Start(bool isServer)
+        {
+            /* Start the server or client on it's own thread. */
+            
+            if (isServer)
+            {
+                Server s = new Server();
+                await Task.Run(() => s.Start());
+                ;
+            }
+            else
+            {
+                Client c = new Client();
+                await Task.Run(() => c.Start());
+                ;
+            }
+        }
+    }
+
+    public class Server
+    {
+        private Connection _client;
+        
+        public async Task Start()
+        {
+            await AwaitConnection();
+        }
+
+        public async Task AwaitConnection()
+        {
+            TcpListener tcpListener = new TcpListener(IPAddress.Any, 6789);
+            tcpListener.Start();
+            
+            TcpClient client = await tcpListener.AcceptTcpClientAsync();
+            
+            _client = new Connection(client);
+            
+            // Start listen loop on own thread
+            Task.Run(ListenLoop).ConfigureAwait(false);
+            
+            // When a connection is accepted, execution continues
+        }
+        
+        public async Task ListenLoop()
+        {
+            await _client.Read();
+            Console.WriteLine("Finished read");
+        }
+    }
+
+    public class Client
+    {
+        private Connection _client;
+        
+        public void Start()
+        {
+            TcpClient tcpClient = new TcpClient();
+            tcpClient.Connect(IPAddress.Loopback, 6789);
+            Console.WriteLine("Client connected");
+            
+            _client = new Connection(tcpClient);
+
+            byte[] arr = BitConverter.GetBytes((ushort) 5);
+            _client.WriteBytes(arr);
+
+            byte[] arrData = ByteArrayHelpers.RandomFill(5);
+            _client.WriteBytes(arrData);
+
+            Console.WriteLine("Send" + Convert.ToBase64String(arrData));
+
+        }
+        
+        /*public async Task WriteLoop()
         {
             
-        }
+        }*/
     }
 }
--- AElf.Network.V2/Connection/Connection.cs
+++ AElf.Network.V2/Connection/Connection.cs
@@ -1,14 +1,77 @@
-﻿using System.Net.Sockets;
+﻿using System;
+using System.Net.Sockets;
+using System.Threading.Tasks;
 
 namespace AElf.Network.V2.Connection
 {
     public class Connection
     {
         private TcpClient _tcpClient;
+        private NetworkStream _stream;
 
         public Connection(TcpClient tcpClient)
         {
             _tcpClient = tcpClient;
+            _stream = tcpClient.GetStream();
         }
+        
+        /// <summary>
+        /// Reads the bytes from the stream.
+        /// </summary>
+        public async Task Read()
+        {
+            try
+            {
+                /*while (true)
+                {*/
+                    byte[] sizePrefixe = await ReadBytesAsync(2);
+                    ushort packetLength = BitConverter.ToUInt16(sizePrefixe, 0);
+
+                    Console.WriteLine("received : " + packetLength);
+                    byte[] packetData = await ReadBytesAsync(packetLength);
+                
+                    Console.WriteLine("Read" + Convert.ToBase64String(packetData));
+                
+                //}
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Reading packet from stream");
+            }
+        }
+        
+        /// <summary>
+        /// Reads bytes from the stream.
+        /// </summary>
+        /// <param name="amount">The amount of bytes we want to read.</param>
+        /// <returns>The read bytes.</returns>
+        protected async Task<byte[]> ReadBytesAsync(int amount)
+        {
+            if (amount == 0) 
+                return new byte[0];
+            
+            byte[] requestedBytes = new byte[amount];
+            
+            int receivedIndex = 0;
+            while (receivedIndex < amount)
+            {
+                while (_tcpClient.Available == 0)
+                    await Task.Delay(TimeSpan.FromMilliseconds(5));
+
+                int readAmount = (amount - receivedIndex >= _tcpClient.Available) ? _tcpClient.Available : amount - receivedIndex;
+                
+                await _stream.ReadAsync(requestedBytes, receivedIndex, readAmount);
+                
+                receivedIndex += readAmount;
+            }
+
+            return requestedBytes;
+        }
+
+        public void WriteBytes(byte[] bytes)
+        {
+            _stream.Write(bytes, 0, bytes.Length);
+        }
+        
     }
 }
